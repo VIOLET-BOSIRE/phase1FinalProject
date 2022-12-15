80761f694c2108f833682ac304030826a56f3f2a
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2022-12-15 11:35:17 +0300

    .js file

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ function getScene () {
       .then(response => response.json())
       .then(payloadIterator);
 }
+// iterates the payload to provide individual elements
+function payloadIterator(payload){
+    
+    payload.forEach(renderItem);
+}
+function renderItem(element){
+    const itemContainer = document.createElement('div');
+    itemContainer.setAttribute('class', 'oneSpecies')
+
+    const image = document.createElement('img');
+    image.setAttribute("class", "img")
+    image.src = element.image;
+    itemContainer.appendChild(image);
+    const name = document.createElement('h4');
+    name.innerHTML = `Name: ${element.name}`;
+    itemContainer.appendChild(name);
+
+    const age = document.createElement('h5');
+    age.innerHTML = `Age: ${element.age} years`;
+    itemContainer.appendChild(age);
+
+    
+}
